2773cc0e7bcc54351b6bc4b09e47fc0f87963d2a
AlvaroHClaver <alvarohc99@hotmail.com>
2026-05-19 20:56:14 -0300

    Small spacing adjust

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,6 @@
 export const About = () => {
   return (
-    <section className="w-[80%] mt-28 flex flex-col gap-4">
+    <section className="w-[80%] mt-18 flex flex-col gap-4">
       <div className="flex flex-col gap-1">
         <h1 className="text-3xl font-bold">Seu Nome</h1>
         <p className="text-details-text">Desenvolvedor Web</p>
